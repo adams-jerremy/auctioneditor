94b4dd6456168dd0ac6382e93cb43c92de77b405
varunjain22 <varunjain22@a9bdc416-1ce8-11df-8a43-bb2f30c6be91>
2010-02-19 04:26:22 +0000

    creating a new google appengine project in java

---
+++ auctioneditor/src/auction/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package auction.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback);
+}
