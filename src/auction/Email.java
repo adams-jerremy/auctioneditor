c349837126fad22b5c1b7c3f3e91d5112bb9f003
varunjain22 <varunjain22@a9bdc416-1ce8-11df-8a43-bb2f30c6be91>
2010-03-04 23:50:49 +0000

    checking withe email key

--- src/auction/Email.java
+++ src/auction/Email.java
@@ -1,7 +1,5 @@
 package auction;
 
-import java.util.Date;
-
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
@@ -10,14 +8,24 @@ import javax.jdo.annotations.PrimaryKey;
 import com.google.appengine.api.datastore.Key;
 
 @PersistenceCapable
-class Email {//tracks users - Buyers and Sellers
+public class Email {//tracks users - Buyers and Sellers
     @PrimaryKey
     @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
     private Key key;
+    
     @Persistent
     private String email;
-    public Email(String e) { email = e;}
-    public Key getKey() { return key; }
-    public String getEmail() { return email; }
-    public void setEmail(String e) { this.email = e;}
+    
+    public Email(String e) {
+    	email = e;
+    }
+    public Key getKey() { 
+    	return key; 
+    }
+    public String getEmail() { 
+    	return email; 
+    }
+    public void setEmail(String e) {
+    	this.email = e;
+    }
 }
